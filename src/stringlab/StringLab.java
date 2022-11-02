0be6c7b4d2078f8bee8ebff0ff3ddbdfab46b7f0
chloedgood <115897586+chloedgood@users.noreply.github.com>
2022-11-01 20:05:10 -0400

    initial code

---
+++ src/stringlab/StringLab.java
@@ -0,0 +1,17 @@
+package stringlab;
+
+public class StringLab {
+	public static void capitalize(String word) {
+System.out.println(word.toUpperCase().charAt(0) + word.substring(1).toLowerCase());
+			
+		}
+	
+	public static void main(String[] args) {
+		
+		String a = "blaze";
+		
+		capitalize(a);
+
+	}
+
+}
